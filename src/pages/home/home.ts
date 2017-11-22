6bb58ba9387a3193a92dc2f4149d5650f5f7e096
SachinDRaj <derycksachin@gmail.com>
2017-11-22 14:12:25 -0400

    attempt at instafeed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// var clientId = 'c1c987d33c3e4fe08d758276698afa7b';
+// var userId = 570178205;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
-
+    // Instafeed(clientId,userId);
   }
-
 }
