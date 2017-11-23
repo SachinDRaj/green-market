c737a671a8cdcb4b3524a24fe1b14d6d03f92667
SachinDRaj <derycksachin@gmail.com>
2017-11-23 18:42:54 -0400

    remove unnecessary stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Firebase } from '@ionic-native/firebase';
+// import { Firebase } from '@ionic-native/firebase';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -39,7 +39,6 @@ import { ReversePipe } from '../app/pipes/ReversePipe';
   providers: [
     StatusBar,
     SplashScreen,
-    Firebase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
